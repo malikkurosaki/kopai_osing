81852668edba5ed69f9feff638e556c7d57bdead
malikkurosaki <kurosakiblackangel@gmail.com>
2022-03-14 01:21:16 +0800

    ssl

--- server/index.js
+++ server/index.js
@@ -51,9 +51,11 @@
 
 
 const express = require('express');
-// const app = require("https-localhost")()
-const app  = express();
+const app = require("https-localhost")()
+// const app  = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('views'));
-app.listen(3000)
+app.listen(3000,() => {
+    console.log(`Server is running on port ${3000}`);
+})
