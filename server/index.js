5319b377a5c508de57707c956746f45d99e92ef6
malikkurosaki <kurosakiblackangel@gmail.com>
2022-03-14 00:50:38 +0800

    ketiga4

--- server/index.js
+++ server/index.js
@@ -51,7 +51,8 @@
 
 
 const express = require('express');
-const app = require("https-localhost")()
+// const app = require("https-localhost")()
+const app  = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('views'));
